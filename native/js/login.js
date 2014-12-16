c9d5273e133cfbd964869541a182be30d5c1f1c3
Breezewish <me@breeswish.org>
2014-12-16 14:14:30 +0800

    use jsSHA in dashboard login

--- native/js/login.js
+++ native/js/login.js
@@ -1,14 +1,15 @@
 (function(window)
 {
+    function sha1(text) {
+        var shaObj = new jsSHA(text, 'TEXT');
+        return shaObj.getHash('SHA-1', 'HEX');
+    }
+
     $(document).ready(function() {
-
         $('.role-form').submit(function() {
-
             var pass = $('.role-password').val();
-            $('.role-password').val(Sha1.hash(Sha1.hash(pass)));
-
+            $('.role-password').val(sha1(sha1(pass)));
         });
-
     });
 
 })(window);
